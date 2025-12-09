2a198c4b82bfd914228fbb9a9fa645e2bc92e949
Greg Bair <greg@gregbair.dev>
2025-12-09 17:25:35 -0500

    Added enabled

--- src/FulcrumLabs.Conductor.Modules.Posix.Firewalld/FirewalldException.cs
+++
@@ -1,4 +0,0 @@
-namespace FulcrumLabs.Conductor.Modules.Posix.Firewalld;
-
-public class FirewalldException(string? message = null, Exception? innerException = null)
-    : Exception(message, innerException);
